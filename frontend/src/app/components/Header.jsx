01032c044b9450a62f1c7c45c5f4d26344e209c7
inuldev <inuldev0@gmail.com>
2025-03-19 02:30:56 +0700

    frontend setup

---
+++ frontend/src/app/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header className="bg-white dark:bg-[rgb(36,37,38)] text-foreground shadow-md h-16 fixed top-0 left-0 right-0 z-50 p-2">
+      Header
+    </header>
+  );
+};
+
+export default Header;
