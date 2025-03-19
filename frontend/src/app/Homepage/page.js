ac0751fb1b2afb0bc5ffaeac4e59bbeb8d935fa3
inuldev <inuldev0@gmail.com>
2025-03-20 06:08:24 +0700

    right sidebar

--- frontend/src/app/Homepage/page.js
+++ frontend/src/app/Homepage/page.js
@@ -1,12 +1,20 @@
+"use client";
+
 import React from "react";
 
 import LeftSideBar from "../components/LeftSideBar";
+import RightSideBar from "../components/RightSideBar";
 
 const HomePage = () => {
   return (
     <div className="flex flex-col min-h-screen bg-background text-foreground">
       <main className="flex flex-1 pt-16">
         <LeftSideBar />
+        <div className="flex-1 px-4 py-6 md:ml-64 lg:mr-64 lg:max-w-2xl xl:max-w-3xl mx-auto"></div>
+
+        <div className="hidden lg:block lg:w-64 xl:w-80 fixed right-0 top-16 bottom-0 overflow-y-auto p-4">
+          <RightSideBar />
+        </div>
       </main>
     </div>
   );
