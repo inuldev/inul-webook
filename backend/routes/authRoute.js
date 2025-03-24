3cfa42efb74f56866322a3ad678d20964131f8c9
inuldev <inuldev0@gmail.com>
2025-03-25 00:28:26 +0700

    google oauth

--- backend/routes/authRoute.js
+++ backend/routes/authRoute.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const router = express.Router();
+const passport = require("passport");
 
+const { generateToken } = require("../utils/tokenGenerator");
 const {
   registerUser,
   loginUser,
@@ -11,5 +13,31 @@ const {
 router.post("/register", registerUser);
 router.post("/login", loginUser);
 router.get("/logout", logout);
+
+//google oauth routes
+router.get(
+  "/google",
+  passport.authenticate("google", {
+    scope: ["profile", "email "],
+  })
+);
+
+//google callback routes
+router.get(
+  "/google/callback",
+  passport.authenticate("google", {
+    failureRedirect: `${process.env.FRONTEND_URL}/user-login`,
+    session: false,
+  }),
+  (req, res) => {
+    const accessToken = generateToken(req?.user);
+    res.cookie("auth_token", accessToken, {
+      httpOnly: true,
+      sameSite: "none",
+      secure: true,
+    });
+    res.redirect(`${process.env.FRONTEND_URL}`);
+  }
+);
 
 module.exports = router;
