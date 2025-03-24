aa10ca6b11bdbd0a3a212ae11d13159625f77907
inuldev <inuldev0@gmail.com>
2025-03-24 21:50:29 +0700

    restfull api for users

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,20 @@
 const express = require("express");
 const cookieParser = require("cookie-parser");
+
 const connectDb = require("./config/db");
 
 require("dotenv").config();
+
+const authRoute = require("./routes/authRoute");
 
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
 
 connectDb();
+
+//api route
+app.use("/auth", authRoute);
 
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => console.log(`server listening on ${PORT}`));
